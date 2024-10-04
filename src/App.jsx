41c6d57afde78fd7a86fba0b2f34d5be82f84a81
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2024-10-04 17:31:28 +0800

    complete exercise 5.1

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])  
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log(`Successfully log in with ${user.username}`)
+    } catch (exception) {
+      console.log('Wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+      username <input type="text" value={username} name='Username' onChange={({target})=>setUsername(target.value)}/>
+      </div>
+      <div>
+      password <input type="text" value={password} name='Password' onChange={({target})=>setPassword(target.value)}/>
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>blogs</h2>
+      <div><p>{user.name} logged in</p></div>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
